c7b896a1f003c00ccfd948376d6fdeea1d156f9b
Vanessa Jesik <vajesik@gmail.com>
2023-10-25 12:35:37 -0600

    completed lab

--- server/app.py
+++ server/app.py
@@ -7,9 +7,9 @@ from flask_restful import Api, Resource
 from models import db, Article, User
 
 app = Flask(__name__)
-app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = b"Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
@@ -18,40 +18,67 @@ db.init_app(app)
 
 api = Api(app)
 
-class ClearSession(Resource):
 
+class ClearSession(Resource):
     def delete(self):
-    
-        session['page_views'] = None
-        session['user_id'] = None
+        session["page_views"] = None
+        session["user_id"] = None
 
         return {}, 204
 
+
 class IndexArticle(Resource):
-    
     def get(self):
         articles = [article.to_dict() for article in Article.query.all()]
         return articles, 200
 
+
 class ShowArticle(Resource):
-
     def get(self, id):
-        session['page_views'] = 0 if not session.get('page_views') else session.get('page_views')
-        session['page_views'] += 1
+        session["page_views"] = (
+            0 if not session.get("page_views") else session.get("page_views")
+        )
+        session["page_views"] += 1
 
-        if session['page_views'] <= 3:
-
+        if session["page_views"] <= 3:
             article = Article.query.filter(Article.id == id).first()
             article_json = jsonify(article.to_dict())
 
             return make_response(article_json, 200)
 
-        return {'message': 'Maximum pageview limit reached'}, 401
-
-api.add_resource(ClearSession, '/clear')
-api.add_resource(IndexArticle, '/articles')
-api.add_resource(ShowArticle, '/articles/<int:id>')
+        return {"message": "Maximum pageview limit reached"}, 401
 
 
-if __name__ == '__main__':
+class Login(Resource):
+    def post(self):
+        username = request.get_json()["username"]
+        user = User.query.filter(User.username == username).first()
+        session["user_id"] = user.id
+        return make_response(user.to_dict(), 200)
+
+
+class Logout(Resource):
+    def delete(self):
+        session["user_id"] = None
+        return make_response("", 204)
+
+
+class CheckSession(Resource):
+    def get(self):
+        user_id = session["user_id"]
+        if user_id:
+            user = db.session.get(User, user_id)
+            return make_response(user.to_dict(), 200)
+        return make_response({}, 401)
+
+
+api.add_resource(ClearSession, "/clear")
+api.add_resource(IndexArticle, "/articles")
+api.add_resource(ShowArticle, "/articles/<int:id>")
+api.add_resource(Login, "/login")
+api.add_resource(Logout, "/logout")
+api.add_resource(CheckSession, "/check_session")
+
+
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
